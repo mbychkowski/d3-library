8cadba976db45c06cc6936477f8fb3046b723c77
michael bychkowski <mbychkowski@gmail.com>
2018-12-09 19:59:59 -0500

    Get svg to print on screen from creating instance of the rectangle class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import shapes from './shapes/shapes';
 
 const triangle = new shapes.Triangle(5,10);
+const square = new shapes.Rectangle(100, 100);
 
 console.log('Hello Shapes');
 
-console.log(triangle.height);
-console.log(triangle.area);
+console.log(triangle.calcArea());
+
+square.drawShape();
--- src/shapes/rectangle/Rectangle.js
+++ src/shapes/rectangle/Rectangle.js
@@ -1,14 +1,30 @@
+import * as d3 from 'd3';
+
 export default class Rectangle {
     constructor(width, height) {
         this.width = width;
         this.height = height;
     }
 
-    get area () {
+    get area() {
         return this.calcArea();
     }
 
-    calcArea () {
+    calcArea() {
         return this.width * this.height;
+    }
+
+    drawShape() {
+        var svg = d3.select('body')
+            .append('svg')
+            .attr('width', this.width)
+            .attr('height', this.height);
+        
+        svg.append('rect')
+            .attr('x', 10) 
+            .attr('y', 10)            
+            .attr('width', this.width)
+            .attr('height', this.height)
+            .attr('fill', 'red');
     }
 }
